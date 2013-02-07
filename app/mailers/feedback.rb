acef54474f085f4befb989e65f072af8bf6194f3
Andrew <darksundarksun@gmail.com>
2013-02-07 23:21:07 +0200

    changed feedback email

--- app/mailers/feedback.rb
+++ app/mailers/feedback.rb
@@ -2,11 +2,11 @@
 class Feedback < ActionMailer::Base
 
   default from: "feedback@ra-kris.com"
-  default to:   "darksundarksun@gmail.com"
+  default to:   "kris@ra-kris.com"
 
   def feedback_email(message)
   	@message = message
-  	mail( :subject =>  "/Повідомлення з ra-kris.com/ #{message.subject}",
+  	mail( :subject =>  "Повідомлення з ra-kris.com -  #{message.subject}",
   		  :body => "Контакти:#{message.contacts} 
   		  			Ім’я:#{message.name} 
   		  			#{message.body}")
