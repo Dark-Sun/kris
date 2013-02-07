a73c99075a6445f8d4d77769a2f2fa4ad95c1b09
Andrew <darksundarksun@gmail.com>
2013-02-08 00:28:55 +0200

    ff

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,11 +1,11 @@
 # -*- encoding : utf-8 -*-
 class Advert < ActiveRecord::Base
   attr_accessible :adress, :category, :city, :desc, :map_path, :name, :image_path, :map, :image
-  has_attached_file :map, 
+  has_attached_file :map, :styles => {:original => '350x265'},
   					:url => "/assets/:id/:style/:basename.:extension",
   					:path => ":rails_root/app/assets/images/:id/:style/:basename.:extension"
 
-  has_attached_file :image, :styles => {:normal => '350x265', :small => '250x250'},
+  has_attached_file :image, :styles => {:original => '350x265'},
   					:url => "/assets/:style/:basename.:extension",
   					:path => ":rails_root/app/assets/images/:style/:basename.:extension"
 
