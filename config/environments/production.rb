b45d86521363b8f487d64315caaa3418e2f465f4
Andrew <darksundarksun@gmail.com>
2013-02-09 21:53:36 +0200

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,7 @@ config.assets.precompile += ['stylesheet.css']
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #turn of  errors
+  config.middleware.delete(ActionDispatch::DebugExceptions)
 end
