188b63ee423c9d6b02b4eaf58738869c9b73c32e
Andrew <darksundarksun@gmail.com>
2013-02-12 00:23:28 +0200

    a

---
+++ db/migrate/20130211222231_advert_change_column_2.rb
@@ -0,0 +1,8 @@
+class AdvertChangeColumn2 < ActiveRecord::Migration
+  def up
+  	change_column :adverts, :adress, :text, :limit => nil
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130211221021) do
+ActiveRecord::Schema.define(:version => 20130211222231) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(:version => 20130211221021) do
     t.string   "map_path"
     t.text     "desc"
     t.string   "category"
-    t.string   "adress"
+    t.text     "adress"
     t.string   "city"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
