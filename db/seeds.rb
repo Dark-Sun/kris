f1280ab410662dea8e60dc8a89ad5c2e4a050f99
Andrew <darksundarksun@gmail.com>
2013-02-12 12:24:20 +0200

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ def get_desc(path)
 	desc
 end
 
-113.times do |n|
+114.times do |n|
 	adv = Advert.new
 	adv.name = "Щит N #{n+1}"
 	adv.city = "Ужгород"
@@ -43,6 +43,87 @@ end
 	adv.desc  = get_desc(Rails.root.to_s + "/app/assets/images/old/u#{n+1}.html")
 	adv.image = load_image("u", n)
 	adv.map = load_image("umape", n)
+	adv.category = "Біл-борд"
+	adv.save
+end
+
+81.times do |n|
+	adv = Advert.new
+	adv.name = "Щит N #{n+1}"
+	adv.city = "Мукачево"
+	adv.adress = get_adress(Rails.root.to_s + "/app/assets/images/old/m#{n+1}.html")
+	adv.adress.slice!("Щит №#{n+1}. ")
+	adv.desc  = get_desc(Rails.root.to_s + "/app/assets/images/old/m#{n+1}.html")
+	adv.image = load_image("m", n)
+	adv.map = load_image("mmape", n)
+	adv.category = "Біл-борд"
+	adv.save
+end
+
+3.times do |n|
+	adv = Advert.new
+	adv.name = "Щит N #{n+1}"
+	adv.city = "Берегово"
+	adv.adress = get_adress(Rails.root.to_s + "/app/assets/images/old/b#{n+1}.html")
+	adv.adress.slice!("Щит №#{n+1}. ")
+	adv.desc  = get_desc(Rails.root.to_s + "/app/assets/images/old/b#{n+1}.html")
+	adv.image = load_image("b", n)
+	adv.map = load_image("bmape", n)
+	adv.category = "Біл-борд"
+	adv.save
+end
+
+7.times do |n|
+	adv = Advert.new
+	adv.name = "Щит N #{n+1}"
+	adv.city = "Чоп"
+	adv.adress = get_adress(Rails.root.to_s + "/app/assets/images/old/c#{n+1}.html")
+	adv.adress.slice!("Щит №#{n+1}. ")
+	adv.desc  = get_desc(Rails.root.to_s + "/app/assets/images/old/c#{n+1}.html")
+	adv.image = load_image("c", n)
+	adv.map = load_image("cmape", n)
+	adv.category = "Біл-борд"
+	adv.save
+end
+
+2.times do |n|
+	7.times do |n|
+	adv = Advert.new
+	adv.name = "Щит N #{n+1}"
+	adv.city = "Свалява"
+	adv.adress = get_adress(Rails.root.to_s + "/app/assets/images/old/s#{n+1}.html")
+	adv.adress.slice!("Щит №#{n+1}. ")
+	adv.desc  = get_desc(Rails.root.to_s + "/app/assets/images/old/s#{n+1}.html")
+	adv.image = load_image("s", n)
+	adv.map = load_image("smape", n)
+	adv.category = "Біл-борд"
+	adv.save
+end
+
+5.times do |n|
+	7.times do |n|
+	adv = Advert.new
+	adv.name = "Щит N #{n+1}"
+	adv.city = "Іршава"
+	adv.adress = get_adress(Rails.root.to_s + "/app/assets/images/old/i#{n+1}.html")
+	adv.adress.slice!("Щит №#{n+1}. ")
+	adv.desc  = get_desc(Rails.root.to_s + "/app/assets/images/old/i#{n+1}.html")
+	adv.image = load_image("i", n)
+	adv.map = load_image("imape", n)
+	adv.category = "Біл-борд"
+	adv.save
+end
+
+2.times do |n|
+	7.times do |n|
+	adv = Advert.new
+	adv.name = "Щит N #{n+1}"
+	adv.city = "Нижні ворота"
+	adv.adress = get_adress(Rails.root.to_s + "/app/assets/images/old/n#{n+1}.html")
+	adv.adress.slice!("Щит №#{n+1}. ")
+	adv.desc  = get_desc(Rails.root.to_s + "/app/assets/images/old/n#{n+1}.html")
+	adv.image = load_image("n", n)
+	adv.map = load_image("nmape", n)
 	adv.category = "Біл-борд"
 	adv.save
 end
